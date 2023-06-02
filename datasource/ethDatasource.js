faab25ea5df44e53478fec7cfdcb2b527b864727
unknown <yecid3257@live.com>
2023-06-01 19:18:23 -0500

    GraphQL with Etherscan, Done

--- datasource/ethDatasource.js
+++ datasource/ethDatasource.js
@@ -1,5 +1,6 @@
 const { RESTDataSource } = require("apollo-datasource-rest");
 
+
 //Vitalik's Ethereum Address
 const eth_address = "0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045";
 
@@ -14,14 +15,18 @@ class EtherDataSource extends RESTDataSource {
     return this
       .get
       //Insert API Endpoint - For Get Ether Balance for a Single Address
-      ();
+      (
+      `?module=account&action=balance&address=${eth_address}&tag=latest&apikey=${process.env.ETHERSCAN_API}`
+      );
   }
 
   async totalSupplyOfEther() {
     return this
       .get
       //Insert API Endpoint - For Get Total Supply of Ether
-      ();
+      (
+        `?module=stats&action=ethsupply&apikey=${process.env.ETHERSCAN_API}`
+      );
   }
 }
 
